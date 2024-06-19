4899eb238040f282b73b07999686cef7e2053963
2022051615100AiLeYan <2486101990@qq.com>
2024-06-19 18:08:18 +0800

    实现三个界面的切换(刚才加了build)

--- build/Desktop_Qt_6_7_1-Debug/CMakeFiles/appPlanes.dir/appPlanes_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appPlanes.dir/appPlanes_autogen/mocs_compilation.cpp.o: \
-  /root/Planes/build/Desktop_Qt_6_7_1-Debug/appPlanes_autogen/mocs_compilation.cpp
--- build/Desktop_Qt_6_7_1-Debug/CMakeFiles/appPlanes.dir/build/Desktop_Qt_6_7_1-Debug/.qt/rcc/qrc_qmake_Planes.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appPlanes.dir/build/Desktop_Qt_6_7_1-Debug/.qt/rcc/qrc_qmake_Planes.cpp.o: \
-  /root/Planes/build/Desktop_Qt_6_7_1-Debug/.qt/rcc/qrc_qmake_Planes.cpp
