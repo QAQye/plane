cc98bbc5d7fff7af5ae2abc3e46a66938129c5c7
2022051615028pengjiale <3112534620@qq.com>
2024-06-19 09:35:20 +0800

    02

--- build/Desktop_Qt_6_7_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_appPlanes_FILE /root/111/plane/build/Desktop_Qt_6_7_1-Debug/appPlanes)
--- build/Desktop_Qt_6_7_1-Debug/CMakeFiles/appPlanes_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/qml_imports/appPlanes_build.cmake"
-  "CMakeFiles/appPlanes_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appPlanes_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
