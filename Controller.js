fb488500bba1c1ecdf1174fc754863c08829afeb
2022051615100AiLeYan <2486101990@qq.com>
2024-06-26 12:03:12 +0800

    实现了基本元素的碰撞检测

--- Controller.js
+++ Controller.js
@@ -14,6 +14,21 @@ let orangejump=[]
 let orangefly=[]
 let orangeplane=[]
 
+
+let bluestartx
+let bluestarty
+let bluepath=[]
+let bluejump=[]
+let bluefly=[]
+let blueplane=[]
+
+let redstartx
+let redstarty
+let redpath=[]
+let redjump=[]
+let redfly=[]
+let redplane=[]
+
 let longs
 let nextposition
 
@@ -272,6 +287,8 @@ function initmap(){
 
 
 
+
+
     // 绿色飞机
     greenplane=[ content.gamewindow.green1, content.gamewindow.green2, content.gamewindow.green3, content.gamewindow.green4]
     // 存入第一架飞机的起始点
@@ -309,7 +326,7 @@ function initmap(){
         // 存入第一架飞机的起始点
         a={"x":content.gamewindow.orange1.startplanex,"y":content.gamewindow.orange1.startplaney}
         orangepath.push(a)
-        a={"x":content.gamewindow.orange1.startplanex,"y":content.gamewindow.orange1.startplaney+longs*2}
+        a={"x":content.gamewindow.orange1.startplanex*4,"y":content.gamewindow.orange4.startplaney}
         orangepath.push(a)
         content.gamewindow.orange1.planepath=orangepath
         content.gamewindow.orange2.planepath=orangepath
@@ -326,6 +343,57 @@ function initmap(){
         orangejump.push(orangepath[i])
     }
      orangefly.push(orangepath[19])
+
+    // 蓝色飞机
+    blueplane=[content.gamewindow.blue1,content.gamewindow.blue2,content.gamewindow.blue3,content.gamewindow.blue4]
+    a={"x":content.gamewindow.blue1.startplanex,"y":content.gamewindow.blue1.startplaney}
+    bluepath.push(a)
+    a={"x":content.gamewindow.blue2.startplanex,"y":content.gamewindow.blue2.startplaney+longs}
+    bluepath.push(a)
+    content.gamewindow.blue1.planepath=bluepath
+    content.gamewindow.blue2.planepath=bluepath
+    content.gamewindow.blue3.planepath=bluepath
+    content.gamewindow.blue4.planepath=bluepath
+    // content.gamewindow.blue1.planepath.push(a)
+    for(let i=14;i>=1;i--){
+        bluepath[16-i]=paths[i]
+    }
+    for(let i=52;i>=17;i--){
+        bluepath[68-i]=paths[i]
+    }
+    for(let i=59;i<=64;i++){
+        bluepath[i-7]=paths[i]
+    }
+    for(let i=3;i<=51;i=i+4){
+        bluejump.push(bluepath[i])
+    }
+    bluefly.push(bluepath[19])
+
+    // 红色飞机
+
+    redplane=[content.gamewindow.red1,content.gamewindow.red2,content.gamewindow.red3,content.gamewindow.red4]
+    a={"x":content.gamewindow.red1.startplanex,"y":content.gamewindow.red1.startplaney}
+    redpath.push(a)
+    a={"x":content.gamewindow.red2.startplanex,"y":content.gamewindow.red2.startplaney+longs}
+    redpath.push(a)
+    content.gamewindow.red1.planepath=redpath
+    content.gamewindow.red2.planepath=redpath
+    content.gamewindow.red3.planepath=redpath
+    content.gamewindow.red4.planepath=redpath
+    for(let i=27;i>=1;i--){
+        redpath[29-i]=paths[i]
+    }
+    for(let i=52;i>=30;i--){
+        redpath[81-i]=paths[i]
+    }
+    for(let i=65;i<=70;i++){
+        redpath[i-13]=paths[i]
+    }
+    for(let i=3;i<=51;i=i+4){
+        redjump.push(redpath[i])
+    }
+    redfly.push(redpath[19])
+
 
 
 
@@ -346,9 +414,11 @@ function initmap(){
                                                                      }
                                                                      else if(clickdoule%4===3){
                                                                          type=3
+                                                                         resetPlanesblue()
                                                                      }
                                                                      else{
                                                                          type=4
+                                                                         resetPlanesred()
                                                                      }
                                                                      console.log("刷新")
                                                                  })
@@ -366,6 +436,7 @@ function initmap(){
     }
 
 
+    // 橙色飞机
     for(let j=0;j<4;j++){
         orangeplane[j].ismoveed.connect(()=>{
                                            for(let k=0;k<4;k++){
@@ -374,6 +445,32 @@ function initmap(){
                                                }
                                            }
                                            moveorangeplane(j)
+
+                                                 })
+    }
+    // 蓝色飞机
+
+    for(let j=0;j<4;j++){
+        blueplane[j].ismoveed.connect(()=>{
+                                           for(let k=0;k<4;k++){
+                                               if(k!==j){
+                                                  blueplane[k].ismove=false
+                                               }
+                                           }
+                                           moveblueplane(j)
+
+                                                 })
+    }
+
+    // 红色飞机
+    for(let j=0;j<4;j++){
+        redplane[j].ismoveed.connect(()=>{
+                                           for(let k=0;k<4;k++){
+                                               if(k!==j){
+                                                  redplane[k].ismove=false
+                                               }
+                                           }
+                                           moveredplane(j)
 
                                                  })
     }
@@ -392,11 +489,17 @@ function resetPlanesorange(){
        orangeplane[i].ismove=true
     }
 }
-function cantPlanesgreen(){
-    for (let i=0;i<greenplane.length;i++) {
-        greenplane[i].ismove=false
+function resetPlanesblue(){
+    for (let i=0;i<blueplane.length;i++) {
+       blueplane[i].ismove=true
     }
 }
+function resetPlanesred(){
+    for (let i=0;i<redplane.length;i++) {
+       redplane[i].ismove=true
+    }
+}
+
 // 移动飞机
 function moveplane(j){
     if(type==1){console.log("绿色飞机")}
@@ -434,6 +537,7 @@ function moveplane(j){
        greenplane[j].currentposition=nextposition
         greenplane[j].x=greenpath[nextposition].x
         greenplane[j].y=greenpath[nextposition].y
+        checkCollisionAndReset(greenplane[j])
 
     }
     // 判断飞机是否能够起飞只有摇骰子摇到六点才能起飞
@@ -469,7 +573,6 @@ function  moveorangeplane(j){
 
        for(let i=0;i<orangejump.length;i++){
 
-
               if(orangepath[nextposition]===orangejump[i]&&nextposition<51){
                   nextposition+=4
                   break;
@@ -495,6 +598,7 @@ function  moveorangeplane(j){
        orangeplane[j].currentposition=nextposition
         orangeplane[j].x=orangepath[nextposition].x
         orangeplane[j].y=orangepath[nextposition].y
+        checkCollisionAndReset(orangeplane[j])
 
     }
     // 判断飞机是否能够起飞只有摇骰子摇到六点才能起飞
@@ -517,4 +621,169 @@ function  moveorangeplane(j){
      }
     // 只能走一次
     orangeplane[j].ismove=false
+}
+
+function moveblueplane(j){
+    if(type==3){console.log("蓝色飞机")}
+     // 判断是否能够起飞以及是否达到了终点和判断这个飞机是否可以移动
+    if(blueplane[j].isfly===true&&blueplane[j].isend===false&&blueplane[j].ismove===true&&type===3){
+       blueplane[j].nextposition=blueplane[j].currentposition+randomNum
+        nextposition=blueplane[j].nextposition
+        // 判断下一次摇的骰子是否达到了终点
+
+       for(let i=0;i<bluejump.length;i++){
+
+
+              if(bluepath[nextposition]===bluejump[i]&&nextposition<51){
+                  nextposition+=4
+                  break;
+              }
+            if(bluepath[nextposition]===bluefly[0]){
+                nextposition=31
+                break;
+            }
+
+          }
+        // 实现二次跳跃
+        if(bluepath[nextposition]===bluefly[0]){
+            nextposition=31
+        }
+        if(nextposition>=57){
+            nextposition=57
+            blueplane[j].isend=true
+        }
+
+
+
+
+       blueplane[j].currentposition=nextposition
+        blueplane[j].x=bluepath[nextposition].x
+        blueplane[j].y=bluepath[nextposition].y
+        checkCollisionAndReset(blueplane[j])
+
+    }
+    // 判断飞机是否能够起飞只有摇骰子摇到六点才能起飞
+    if(blueplane[j].isfly===false&&randomNum===6&&blueplane[j].ismove===true&&type===3){
+       blueplane[j].nextposition=blueplane[j].currentposition+1
+        nextposition=blueplane[j].nextposition
+        blueplane[j].currentposition=nextposition
+        blueplane[j].x=bluepath[nextposition].x
+       blueplane[j].y=bluepath[nextposition].y
+        blueplane[j].isfly=true
+    }
+    // 此时场上有同颜色飞机处于起飞状态但是没有摇到点数6选择了未起飞状态的飞机
+     if(blueplane[j].isfly===false&&type===3){
+         for(let k=0;k<3;k++){
+             if(blueplane[k].isfly){
+                     // 如果飞机不能起飞但是选择了那么需要重新进行选择
+                     blueplane[k].ismove=true
+                 }
+             }
+     }
+    // 只能走一次
+    blueplane[j].ismove=false
+}
+
+
+function moveredplane(j){
+
+    if(type==4){console.log("红色飞机")}
+     // 判断是否能够起飞以及是否达到了终点和判断这个飞机是否可以移动
+    if(redplane[j].isfly===true&&redplane[j].isend===false&&redplane[j].ismove===true&&type===4){
+       redplane[j].nextposition=redplane[j].currentposition+randomNum
+        nextposition=redplane[j].nextposition
+        // 判断下一次摇的骰子是否达到了终点
+
+       for(let i=0;i<redjump.length;i++){
+
+
+              if(redpath[nextposition]===redjump[i]&&nextposition<51){
+                  nextposition+=4
+                  break;
+              }
+            if(redpath[nextposition]===redfly[0]){
+                nextposition=31
+                break;
+            }
+
+          }
+        // 实现二次跳跃
+        if(redpath[nextposition]===redfly[0]){
+            nextposition=31
+        }
+        if(nextposition>=57){
+            nextposition=57
+            redplane[j].isend=true
+        }
+
+
+
+
+       redplane[j].currentposition=nextposition
+        redplane[j].x=redpath[nextposition].x
+        redplane[j].y=redpath[nextposition].y
+        checkCollisionAndReset(redplane[j])
+
+    }
+    // 判断飞机是否能够起飞只有摇骰子摇到六点才能起飞
+    if(redplane[j].isfly===false&&randomNum===6&&redplane[j].ismove===true&&type===4){
+       redplane[j].nextposition=redplane[j].currentposition+1
+        nextposition=redplane[j].nextposition
+        redplane[j].currentposition=nextposition
+        redplane[j].x=redpath[nextposition].x
+       redplane[j].y=redpath[nextposition].y
+        redplane[j].isfly=true
+    }
+    // 此时场上有同颜色飞机处于起飞状态但是没有摇到点数6选择了未起飞状态的飞机
+     if(redplane[j].isfly===false&&type===4){
+         for(let k=0;k<3;k++){
+             if(redplane[k].isfly){
+                     // 如果飞机不能起飞但是选择了那么需要重新进行选择
+                     redplane[k].ismove=true
+                 }
+             }
+     }
+    // 只能走一次
+    redplane[j].ismove=false
+     // 检测碰撞
+
+}
+// 定义一个检查碰撞并重置飞机位置的函数
+function checkCollisionAndReset(plane) {
+    let planes = [
+        content.gamewindow.green1,
+        content.gamewindow.green2,
+        content.gamewindow.green3,
+        content.gamewindow.green4,
+        content.gamewindow.orange1,
+        content.gamewindow.orange2,
+        content.gamewindow.orange3,
+        content.gamewindow.orange4,
+        content.gamewindow.blue1,
+        content.gamewindow.blue2,
+        content.gamewindow.blue3,
+        content.gamewindow.blue4,
+        content.gamewindow.red1,
+        content.gamewindow.red2,
+        content.gamewindow.red3,
+        content.gamewindow.red4
+    ]
+    plane.ismove=false
+    for (let i = 0; i < planes.length; i++) {
+            if (
+                planes[i].planepath[planes[i].currentposition].x === plane.planepath[plane.currentposition].x &&
+                planes[i].planepath[planes[i].currentposition].y === plane.planepath[plane.currentposition].y &&
+                planes[i].type !== plane.type) { // 仅当飞机颜色不同才重置位置
+                resetPlane(planes[i]);
+            }
+        }
+}
+
+// 重置飞机位置的函数
+function resetPlane(plane) {
+    // 让其回到起点位置和终点位置并且设置飞机的状态是不可起飞
+    plane.currentposition = 0
+    plane.isfly=false
+    plane.x = plane.startplanex
+    plane.y = plane.startplaney
 }
