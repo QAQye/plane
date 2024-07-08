61149178febd6e66b844d7ef08a1cb1b350f4a3d
2022051615028pengjiale <3112534620@qq.com>
2024-07-08 10:45:38 +0800

    网上学习后更新的客户端代码提交

--- build/Desktop_Qt_6_7_1-Debug/Planes/PlaneController.js
+++
@@ -1,6 +0,0 @@
-function printPlaneDetails(planeSize, startPlaneX, startPlaneY) {
-    // console.log("Plane ID:", planeId);
-    console.log("Plane Size:", planeSize);
-    console.log("Start Plane X:", startPlaneX);
-    console.log("Start Plane Y:", startPlaneY);
-}
